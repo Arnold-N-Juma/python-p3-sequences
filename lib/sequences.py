6c9e284e4e6376bce6b46f0c2945dfd842506a6e
Arnold Juma <+>
2023-11-24 02:08:56 +0300

    Working Ok

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        print([])
+        return 
+
+    list = [0, 1]
+
+    while len(list) < length:
+        next_number = list[-1] + list[-2]
+        list.append(next_number)
+
+    print(list[:length])
+pass
